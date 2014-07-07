511fd90070b99176171f7eae144733022a883df5
Jenny <caretdashcaret@gmail.com>
2014-07-06 21:47:14 -0400

    Extrude bridge
    Translate the bridge for protrusion.
    
    Refactor functions invovling bisection for reusability.

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import bpy
 
-def extrude_curve(selected_curve):
+def extrude_curve(selected_object):
     """create the basic extrusion from curve"""
     bpy.ops.object.origin_set(type="GEOMETRY_ORIGIN") #move the object to the center
     
-    selected_curve.extrude = 0.005 #extrude path to create mesh
-    #selected_curve.bevel_depth = 0.001 #bevel the edges
+    path = selected_object.data #path data of the glasses
+    
+    path.extrude = 0.005 #extrude path to create mesh
+    #path.bevel_depth = 0.001 #bevel the edges
 
     bpy.ops.object.convert(target='MESH', keep_original=False) #convert from curve to mesh
     
@@ -21,9 +23,7 @@ def move_to_origin(selected_object):
     """move the selected object to origin"""
     bpy.ops.object.mode_set(mode="OBJECT")
     
-    current_location = selected_object.location
-    translation_vector = (-1*current_location[0], -1*current_location[1], -1*current_location[2]) #move to global origin
-    bpy.ops.transform.translate(value=translation_vector)
+    selected_object.location = (0.0, 0.0, 0.0)
     
 def bisect(midpoint, number_of_segments, spacing):
     """bisect area around the midpoint, based on the number of segments and spacing"""
@@ -49,7 +49,7 @@ def bend_object(selected_object):
     bpy.ops.object.modifier_add(type="SIMPLE_DEFORM") #add simple deform modifier
     simple_deform_modifier = selected_object.modifiers["SimpleDeform"]
     simple_deform_modifier.deform_method = "BEND"
-    simple_deform_modifier.angle = 0.523599 #Bend 30 degrees
+    simple_deform_modifier.angle = 0.436332 #Bend 25 degrees
 
     bpy.ops.object.modifier_apply(apply_as="DATA", modifier="SimpleDeform") #apply modifier
     
@@ -58,20 +58,44 @@ def scale(value=100.0):
     bpy.ops.object.mode_set(mode="OBJECT")
     bpy.ops.transform.resize(value=(100.0, 100.0, 100.0)) #scale 100x for easier translations
 
-def bisect_left_lens_area():
-    bisect(midpoint=-8.0, number_of_segments=5, spacing=1.0)
-def bisect_right_lens_area():
-    bisect(midpoint=8.0, number_of_segments=5, spacing=1.0)
-
 def bisect_mid_lens_areas():
-    bisect_left_lens_area()
-    bisect_right_lens_area()
+    """bisect the area around the middle of each lens to lessen the artifacts of transformations"""
+    left_lens_area = -8.0
+    right_lens_area = 8.0
+    
+    for point in [left_lens_area, right_lens_area]:
+        bisect(midpoint=point, number_of_segments=5, spacing=1.0)
+        
+def select_mid_bridge_points(selected_object, delta=0.0025):
+    """select the points around the middle section of the bridge, select only works if the mode is OBJECT while selecting, and then changed to EDIT for subsequent operations"""
+    
+    bpy.ops.object.mode_set(mode="OBJECT")
+    mesh = selected_object.data #mesh data of the selected object
+    
+    #iterating through a lot of vertexes :(
+    for vertex in mesh.vertices:
+        x_co = vertex.co[0]
+        if (x_co >= -1 * delta) and (x_co <= delta):
+            vertex.select = True
+            
+def deselect_all(selected_object):
+    """Certain operations will leave vertices selected which will interfere with subsequent operations. This will try to deselect everything."""
+    bpy.ops.object.mode_set(mode="EDIT")
+    mesh = selected_object.data
+    if mesh.total_vert_sel > 0:
+        bpy.ops.mesh.select_all()
+    
+def protrude_bridge():
+    """translate the middle section of the bridge, with propotional selection"""
+    bpy.ops.object.mode_set(mode="EDIT")
+    
+    bpy.ops.transform.translate(value=(0.0, -0.4, 0.0), proportional="ENABLED", proportional_edit_falloff="SMOOTH", proportional_size=0.7)
+    
 
 def run():
     selected_object = bpy.context.scene.objects.active #imported glasses
-    path = selected_object.data #path data of the glasses
 
-    extrude_curve(path)
+    extrude_curve(selected_object)
 
     #Since Bend only bends around the Z axis, the glasses have to be rotated. It needs to be rotated in EDIT mode otherwise the Z will be relative to the original mesh.
     rotate_object()
@@ -89,7 +113,11 @@ def run():
     bisect_mid_lens_areas()
     
     bend_object(selected_object)
-
+    
+    #protrude the upper bridge region
+    deselect_all(selected_object)
+    select_mid_bridge_points(selected_object)
+    protrude_bridge()
 
 run()
 print("Success")
