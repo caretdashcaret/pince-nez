c7e90e1852ceafa14a9e2a8b71eaecdbd3fe87a3
Jenny <caretdashcaret@gmail.com>
2014-07-06 20:42:48 -0400

    Refactor into functions and increase number of bisections
    Refactor into functions for better code maintainence.
    
    Increased number of bisections to reduce artifacts, for a smoother protrusion of the bridge.

--- run.py
+++ run.py
@@ -1,55 +1,95 @@
 import bpy
 
-selected_object = bpy.context.scene.objects.active #imported glasses
-path = selected_object.data #path data of the glasses
+def extrude_curve(selected_curve):
+    """create the basic extrusion from curve"""
+    bpy.ops.object.origin_set(type="GEOMETRY_ORIGIN") #move the object to the center
+    
+    selected_curve.extrude = 0.005 #extrude path to create mesh
+    #selected_curve.bevel_depth = 0.001 #bevel the edges
 
-##Create basic glasses extrusion
+    bpy.ops.object.convert(target='MESH', keep_original=False) #convert from curve to mesh
+    
+def rotate_object():
+    """Rotate the object 90 degrees on the X axis. Blender uses radians so rotate value is 1.570797"""
 
-bpy.ops.object.origin_set(type="GEOMETRY_ORIGIN") #move the object to the center
+    bpy.ops.object.mode_set(mode="EDIT")
+    bpy.ops.mesh.select_all()
+    bpy.ops.transform.rotate(value=1.570797, constraint_axis=(False, True, True)) #rotate 90 degrees only on the X
+    bpy.ops.object.mode_set(mode="OBJECT")
 
-path.extrude = 0.005 #extrude path to create mesh
-#path.bevel_depth = 0.001 #bevel the edges
+def move_to_origin(selected_object):
+    """move the selected object to origin"""
+    bpy.ops.object.mode_set(mode="OBJECT")
+    
+    current_location = selected_object.location
+    translation_vector = (-1*current_location[0], -1*current_location[1], -1*current_location[2]) #move to global origin
+    bpy.ops.transform.translate(value=translation_vector)
+    
+def bisect(midpoint, number_of_segments, spacing):
+    """bisect area around the midpoint, based on the number of segments and spacing"""
+    bpy.ops.object.mode_set(mode="EDIT")
 
-bpy.ops.object.convert(target='MESH', keep_original=False) #convert from curve to mesh
+    lower_range = int(-1 * number_of_segments/2)
+    upper_range = int(number_of_segments/2 + 1)
+    spread = range(lower_range, upper_range)
+    bisection_points = [x * spacing + midpoint for x in spread]
 
-bpy.ops.object.origin_set(type="ORIGIN_CENTER_OF_MASS") #move object origin to center of mass
+    for x_co in bisection_points:
+        bpy.ops.mesh.select_all() #toggle select
+        bpy.ops.mesh.select_all() #toggle select
+        bpy.ops.mesh.bisect(plane_co=(x_co, 0.0, 0.0), plane_no=(1.0, 0.0, 0.0), threshold=0, xstart=0, xend=10, ystart=0, yend=100)
 
-##Bend
+def bisect_bridge_area():
+    """bisect the bridge area into 20 segments"""
+    bisect(midpoint=0.0, number_of_segments=80, spacing=0.025)
 
-#Since Bend only bends around the Z axis, the glasses have to be rotated. It needs to be rotated in EDIT mode otherwise the Z will be relative to the original mesh.
+def bend_object(selected_object):
+    """apply the bend simple deform modifier"""
+    bpy.ops.object.mode_set(mode="OBJECT")
+    bpy.ops.object.modifier_add(type="SIMPLE_DEFORM") #add simple deform modifier
+    simple_deform_modifier = selected_object.modifiers["SimpleDeform"]
+    simple_deform_modifier.deform_method = "BEND"
+    simple_deform_modifier.angle = 0.523599 #Bend 30 degrees
 
-#Blender uses radians internally
+    bpy.ops.object.modifier_apply(apply_as="DATA", modifier="SimpleDeform") #apply modifier
+    
+def scale(value=100.0):
+    """scale the object to 100x the size"""
+    bpy.ops.object.mode_set(mode="OBJECT")
+    bpy.ops.transform.resize(value=(100.0, 100.0, 100.0)) #scale 100x for easier translations
 
-bpy.ops.object.mode_set(mode="EDIT")
-bpy.ops.mesh.select_all()
-bpy.ops.transform.rotate(value=1.570797, constraint_axis=(False, True, True)) #rotate 90 degrees only on the X
-bpy.ops.object.mode_set(mode="OBJECT")
+def bisect_left_lens_area():
+    bisect(midpoint=-8.0, number_of_segments=5, spacing=1.0)
+def bisect_right_lens_area():
+    bisect(midpoint=8.0, number_of_segments=5, spacing=1.0)
 
-bpy.ops.object.modifier_add(type="SIMPLE_DEFORM") #add simple deform modifier
-simple_deform_modifier = selected_object.modifiers["SimpleDeform"]
-simple_deform_modifier.deform_method = "BEND"
-simple_deform_modifier.angle = 0.523599 #Bend 30 degrees
+def bisect_mid_lens_areas():
+    bisect_left_lens_area()
+    bisect_right_lens_area()
 
-bpy.ops.object.modifier_apply(apply_as="DATA", modifier="SimpleDeform") #apply modifier
+def run():
+    selected_object = bpy.context.scene.objects.active #imported glasses
+    path = selected_object.data #path data of the glasses
 
-##Scale
-bpy.ops.transform.resize(value=(100.0, 100.0, 100.0)) #scale 100x for easier translations
+    extrude_curve(path)
 
-##Translate the bridge
+    #Since Bend only bends around the Z axis, the glasses have to be rotated. It needs to be rotated in EDIT mode otherwise the Z will be relative to the original mesh.
+    rotate_object()
 
-current_location = selected_object.location
-translation_vector = (-1*current_location[0], -1*current_location[1], -1*current_location[2]) #move to global origin
-bpy.ops.transform.translate(value=translation_vector)
+    ##Create extra segments to increase detail for Bend as well as protruding the bridge
 
-#Create 
-bpy.ops.object.mode_set(mode="EDIT")
+    bpy.ops.object.origin_set(type="ORIGIN_CENTER_OF_MASS") #move object origin to center of mass
 
-midpoint = 0.0 #midpoint of the glasses, to estimate the bridge
-bisection_points = [x * 0.025 + midpoint for x in range(-10,11)]
+    scale()
+    
+    move_to_origin(selected_object)
 
-for x_co in bisection_points:
-    bpy.ops.mesh.select_all() #toggle select
-    bpy.ops.mesh.select_all() #toggle select
-    bpy.ops.mesh.bisect(plane_co=(x_co, 0.0, 0.0), plane_no=(1.0, 0.0, 0.0), threshold=0, xstart=0, xend=10, ystart=0, yend=100)
+    #the bisects help with future transforms and prevent distortions due to long faces
+    bisect_bridge_area()
+    bisect_mid_lens_areas()
+    
+    bend_object(selected_object)
 
+
+run()
 print("Success")
