ea164b87ba302f76bffd82719e31922ababcef09
Jenny <caretdashcaret@gmail.com>
2014-10-14 20:34:55 -0400

    Refactor to clean up code.

--- script/run.py
+++ script/run.py
@@ -7,6 +7,7 @@ In addition to the arguments, run expects a SVG to be loaded and selected in Ble
 import bpy
 import bmesh
 
+
 def deselect_all_vertices():
     """certain operations will leave vertices selected which will interfere with subsequent operations.
     This will try to deselect everything."""
@@ -44,49 +45,8 @@ def rotate_object():
 def move_to_origin(selected_object):
     """move the selected object to origin"""
     bpy.ops.object.mode_set(mode="OBJECT")
-    
+
     selected_object.location = (0.0, 0.0, 0.0)
-  
-    
-def bisect_midpoints(midpoint, number_of_segments, spacing):
-    """bisect area around an midpoint, based on the number of segments and spacing"""
-    bpy.ops.object.mode_set(mode="EDIT")
-
-    lower_range = int(-1 * number_of_segments/2)
-    upper_range = int(number_of_segments/2 + 1)
-    spread = range(lower_range, upper_range)
-    bisection_points = [x * spacing + midpoint for x in spread]
-
-    for x_coord in bisection_points:
-        bisect(x_coord)
-
-
-def find_half_length(mesh):
-    """returns the length of half of the frame, assumed to be the max x coordinate,
-    since mesh is centered at the global origin"""
-    half_length = max([vertex.co[0] for vertex in mesh.vertices])
-    return half_length
-
-
-def find_bridge_area(half_length):
-    """suppose the bridge area is 16% of the entire length of the frame,
-    only return half of that because it's split across the origin"""
-    return 0.16 * half_length
-    
-
-def bisect_bridge_area(bridge_area):
-    """bisect the bridge area into segments"""
-    deselect_all_vertices()
-    bpy.ops.object.mode_set(mode="EDIT")
-    bpy.ops.mesh.select_all(action="SELECT")
-    
-    segments = 80
-    delta = bridge_area / segments
-    
-    bisect_midpoints(midpoint=0.0, number_of_segments=segments, spacing=delta)
-
-    #the operation actually requires a mode change to take effect
-    bpy.ops.object.mode_set(mode="OBJECT")
 
 
 def bend_object(selected_object, bend_degree):
@@ -104,188 +64,6 @@ def bend_object(selected_object, bend_degree):
     bpy.ops.object.modifier_apply(apply_as="DATA", modifier="SimpleDeform")
 
 
-def scale_for_manipulation(value=100.0):
-    """scale the object to 100x"""
-    deselect_all_vertices()
-    bpy.ops.object.mode_set(mode="EDIT")
-    bpy.ops.mesh.select_all(action="SELECT")
-
-    bpy.ops.transform.resize(value=(100.0, 100.0, 100.0))
-
-
-def find_mid_lens_coord(half_length):
-    """assume the middle of the lens is half way between the middle and one of the ends,
-    return that point"""
-    return half_length/2
-        
-
-def bisect_mid_lens_areas(mid_lens_point, max_x_co):
-    """bisect the area around the middle of each lens"""
-    left_lens_area = -1 * mid_lens_point
-    right_lens_area = mid_lens_point
-
-    #5% of the width of half the frame
-    delta = 0.05 * max_x_co
-    
-    for point in [left_lens_area, right_lens_area]:
-        bisect(midpoint=point, number_of_segments=7, spacing=delta)
-
-
-def select_mid_bridge_points(mesh, delta=0.025):
-    """select the vertices around the middle section of the bridge.
-    The delta should be the same value as the spacing in the bisect_bridge"""
-    deselect_all_vertices()
-    bpy.ops.object.mode_set(mode="OBJECT")
-    
-    region = delta * 3
-
-    for vertex in mesh.vertices:
-        x_co = vertex.co[0]
-        if (x_co >= -1 * region) and (x_co <= region):
-            vertex.select = True
-
-
-def protrude_bridge():
-    """translate the middle section of the bridge, with propotional selection"""
-    bpy.ops.object.mode_set(mode="EDIT")
-    
-    bpy.ops.transform.translate(value=(0.0, -0.4, 0.0), proportional="ENABLED", proportional_edit_falloff="SMOOTH", proportional_size=1.0)
-
-
-def scale_to_lifesize(half_length, lifesize):
-    """scale to the correct size for 3D printing"""
-    deselect_all_vertices()
-    
-    scale = lifesize / (half_length * 2) * 100
-    
-    bpy.ops.object.mode_set(mode="EDIT")
-    bpy.ops.transform.resize(value=(scale, scale, scale))
-
-
-def in_bridge_area(vertex, bridge_area):
-    x,y,z = vertex.co
-    
-    return (x <= bridge_area/2) and (x >= -1 * bridge_area/2)
-
-
-def in_right_nosepad_area(vertex, bridge_area):
-    x,y,z = vertex.co
-    
-    return (x<= -1 * bridge_area/2) and (x >= -1 * bridge_area * 3/2)
-
-
-def in_left_nosepad_area(vertex, bridge_area):
-    x,y,z = vertex.co
-    
-    return (x<= bridge_area * 3/2) and (x >= bridge_area/2)
-
-
-def in_nosepad_bridge_region(vertex, bridge_area):
-    x,y,z = vertex.co
-    
-    return (x<= 2*bridge_area) and (x >= -2 * bridge_area)
-
-
-def find_nosepads_values(mesh, bridge_area):
-    """
-    left nosepad --- bridge area --- right nosepad
-    suppose each nosepad area is the same width as the bridge area
-
-    the bottom_of_bridge and bottom_of_nosepad are used to calculate how high (z) is the peak from the bottom
-    right_nosepad_vertices and left_nosepad_vertices are vertices in the region of the left and right nosepads
-
-    front_vertices are vertices that may be changed by the transformations that create the nosepads
-    and have to be reverted. I think it's easier to revert the changes, than computing a gaussian for figuring out
-    how much to extrude the nosepad around its peak
-    """
-    
-    bottom_of_bridge = 100
-    bottom_of_nosepad = 100
-    
-    right_nosepad_vertices = []
-    left_nosepad_vertices = []
-
-    front_vertices = {}
-    
-    for vertex in mesh.vertices:
-        if in_nosepad_bridge_region(vertex, bridge_area):
-            if in_bridge_area(vertex, bridge_area):
-                height = vertex.co[2]
-                if height < bottom_of_bridge:
-                    bottom_of_bridge = height
-            
-            if in_right_nosepad_area(vertex, bridge_area):
-                #need to store index otherwise can't perform vertex selections due to
-                #loss of reference-of-objects?
-                right_nosepad_vertices.append(vertex.index)
-                #assume the frame is symmetrical, so only need to get the value from one side
-                height = vertex.co[2]
-                if height < bottom_of_nosepad:
-                    bottom_of_nosepad = height
-            
-            if in_left_nosepad_area(vertex, bridge_area):
-                left_nosepad_vertices.append(vertex.index)
-            
-        y_depth = vertex.co[1]
-         #future manipulations could affect a wider area than the region
-        if y_depth <= 0:
-            #extracted for immutability
-            x, y, z = vertex.co
-            front_vertices[vertex.index] = (x,y,z)
-        
-    return [bottom_of_bridge, bottom_of_nosepad], front_vertices, [right_nosepad_vertices, left_nosepad_vertices]
-
-
-def find_nosepad_peak_height(nosepad_z_height_range):
-    """find the height (z) of the set of vertices to select for where the peak of the nosepad is"""
-
-    bottom_of_bridge, bottom_of_nosepad = nosepad_z_height_range
-    
-    distance = bottom_of_bridge - bottom_of_nosepad
-    
-    peak = bottom_of_bridge - 0.4 * distance
-    delta = 0.05 * distance
-    
-    return peak + delta, peak - delta
-
-
-def select_nosepad_peak_vertices(nosepad_peak_z_height_range, nosepad_vertex_indices, mesh):
-    """select the vertices that will become the peak for the nosepad"""
-    deselect_all_vertices()
-    bpy.ops.object.mode_set(mode="OBJECT")
-
-    upper_nosepad_peak, bottom_nosepad_peak = nosepad_peak_z_height_range
-
-    for index in nosepad_vertex_indices:
-        vertex = mesh.vertices[index]
-        height = vertex.co[2]
-        
-        if (height <= upper_nosepad_peak) and (height >= bottom_nosepad_peak):
-            vertex.select = True
-
-
-def extrude_nosepad_peak():
-    """translate the nosepad along the y-axis"""
-    bpy.ops.object.mode_set(mode="EDIT")
-    
-    bpy.ops.transform.translate(value=(0.0, 1.0, 0.25), proportional="ENABLED", proportional_edit_falloff="SMOOTH", proportional_size=2.5)
-
-
-def shrink_nosepad():
-    """make the nosepad thinner than the frame part"""
-    bpy.ops.object.mode_set(mode="EDIT")
-    
-    bpy.ops.transform.shrink_fatten(value=0.2, proportional="ENABLED", proportional_edit_falloff="SMOOTH", proportional_size=1.0)
-
-
-def reset_normal_vertices(normal_vertices, mesh):
-    """set the location of vertices to specified positions"""
-    bpy.ops.object.mode_set(mode="OBJECT")
-    
-    for index, coord in normal_vertices.items():
-        mesh.vertices[index].co = coord
-
-
 def remove_duplicate_vertices():
     """remove duplicate vertices by welding them together"""
     bpy.ops.object.mode_set(mode="EDIT")
@@ -296,32 +74,11 @@ def remove_duplicate_vertices():
     bpy.ops.object.mode_set(mode="OBJECT")
 
 
-def select_all_nosepad_vertices(nosepad_vertex_indices, nosepad_z_height_range, mesh):
-    """select all the points in the nosepad region less than the max nosepad_z_height"""
-    deselect_all_vertices()
-    bpy.ops.object.mode_set(mode="OBJECT")
-
-    upper_bound = nosepad_z_height_range[0]
-
-    for index in nosepad_vertex_indices:
-        vertex = mesh.vertices[index]
-        height = vertex.co[2]
-        
-        if height < upper_bound:
-            vertex.select = True
-
-
-def bisect_origin():
-    """splits down the origin"""
-    bisect(0.0)
-
-
 def move_object_origin_to_center_of_mass():
     """
     move object origin to center of mass
     """
     bpy.ops.object.mode_set(mode="OBJECT")
-
     bpy.ops.object.origin_set(type="ORIGIN_CENTER_OF_MASS")
 
 
@@ -414,16 +171,6 @@ def reorient_for_easier_manipulation(selected_object):
     move_to_origin(selected_object)
 
 
-def create_nosepads(selected_object):
-    #bisect middle to lessen artifacts from creating nosepads
-    #the bisect_bridge_area method isn't called here, because that increases artifacts when scaling the nosepads
-    #it's a delicate balance
-    bisect_origin()
-
-    #create_left_nosepad()
-    #create_right_nosepad()
-
-
 def form_lens_and_bridge(bridge_width, bend_degree):
 
     bridge_object, left_lens_object, right_lens_object = duplicate_object(2)
@@ -436,8 +183,8 @@ def form_lens_and_bridge(bridge_width, bend_degree):
     gap = 0.04 * bridge_object.dimensions[0]
     align(left_lens_object, right_lens_object, bridge_object, gap)
 
-    partial_frame = combine_left_lens_area_and_bridge(left_lens_object, bridge_object)
-    complete_frame = combine_right_lens_area_for_full_frame(right_lens_object, partial_frame)
+    partial_frame = combine_for_frame(left_lens_object, bridge_object, right_two_pieces=False)
+    complete_frame = combine_for_frame(right_lens_object, partial_frame, right_two_pieces=True)
 
     return complete_frame
 
@@ -543,9 +290,7 @@ def cut_bridge(bridge_object, bridge_width):
 
 
 def align_left_lens_area_and_bridge(left_lens_area_object, bridge_object, gap):
-    select_object(bridge_object)
-    select_second_object(left_lens_area_object)
-    bpy.ops.object.align(bb_quality=True, align_mode='OPT_3', relative_to='OPT_1', align_axis={'Y'})
+    align_lens_area_and_bridge(left_lens_area_object, bridge_object)
 
     select_object(bridge_object)
     bpy.ops.transform.translate(value=(gap, 0, 0), constraint_axis=(True, False, False))
@@ -563,17 +308,9 @@ def merge_objects(object_a, object_b):
     return combined_bridge_object
 
 
-def combine_left_lens_area_and_bridge(left_lens_area_object, bridge_object):
-    merged_frame = merge_objects(left_lens_area_object, bridge_object)
-    select_object(merged_frame)
-    bridge_left_gap(merged_frame)
-
-    return merged_frame
-
-
-def bridge_left_gap(merged_frame):
+def bridge_gap_between_lens_area_and_bridge(frame_object, gap_on_the_right_side=True):
     select_non_manifold_vertices()
-    deselect_non_manifold_vertices_on_the_right_side(merged_frame)
+    deselect_non_manifold_vertices(frame_object, deselect_left_side=gap_on_the_right_side)
     bridge_gap()
 
 
@@ -581,27 +318,6 @@ def select_non_manifold_vertices():
     bpy.ops.object.mode_set(mode="EDIT")
     bpy.ops.mesh.select_all(action='DESELECT')
     bpy.ops.mesh.select_non_manifold()
-
-
-def deselect_non_manifold_vertices_on_the_right_side(merged_frame):
-
-    bpy.ops.object.mode_set(mode='EDIT')
-
-    mesh = bmesh.from_edit_mesh(merged_frame.data)
-
-    for edge in mesh.edges:
-        start_vertex, end_vertex = edge.verts
-
-        if on_right_side(start_vertex) or on_right_side(end_vertex):
-            deselect_edge_and_associated_vertices(edge, start_vertex, end_vertex)
-
-        else:
-            pass
-
-    for vertex in mesh.verts:
-        x_coord = vertex.co[0]
-        if x_coord >= 0:
-            vertex.select = False
 
 
 def deselect_edge_and_associated_vertices(edge, start_vertex, end_vertex):
@@ -631,37 +347,12 @@ def bridge_gap():
     bpy.ops.mesh.bridge_edge_loops(use_merge=True, number_cuts=3, smoothness=3)
 
 
-def combine_right_lens_area_for_full_frame(right_lens_object, partial_frame):
-    frame_object = merge_objects(right_lens_object, partial_frame)
+def combine_for_frame(lens_object, bridge_object, right_two_pieces=True):
+    frame_object = merge_objects(lens_object, bridge_object)
     select_object(frame_object)
-    bridge_right_gap(frame_object)
+    bridge_gap_between_lens_area_and_bridge(frame_object, gap_on_the_right_side=right_two_pieces)
 
     return frame_object
-
-
-def bridge_right_gap(frame_object):
-    select_non_manifold_vertices()
-    deselect_non_manifold_vertices_on_the_left_side(frame_object)
-    bridge_gap()
-
-
-def deselect_non_manifold_vertices_on_the_left_side(frame_object):
-    bpy.ops.object.mode_set(mode='EDIT')
-
-    mesh = bmesh.from_edit_mesh(frame_object.data)
-
-    for edge in mesh.edges:
-        start_vertex, end_vertex = edge.verts
-
-        if on_left_side(start_vertex) or on_left_side(end_vertex):
-            deselect_edge_and_associated_vertices(edge, start_vertex, end_vertex)
-
-        else:
-            pass
-
-    for vertex in mesh.verts:
-        if on_left_side(vertex):
-            vertex.select = False
 
 
 def on_left_side(vertex):
@@ -671,13 +362,41 @@ def on_left_side(vertex):
     return False
 
 
-def align_right_lens_area_and_bridge(right_lens_object, bridge_object, gap):
-    select_object(bridge_object)
-    select_second_object(right_lens_object)
-    bpy.ops.object.align(bb_quality=True, align_mode='OPT_3', relative_to='OPT_1', align_axis={'Y'})
+def deselect_non_manifold_vertices(mesh_object, deselect_left_side=True):
 
-    select_object(right_lens_object)
+    side_check = on_right_side
+    if deselect_left_side:
+        side_check = on_left_side
+
+    bpy.ops.object.mode_set(mode='EDIT')
+
+    mesh = bmesh.from_edit_mesh(mesh_object.data)
+
+    for edge in mesh.edges:
+        start_vertex, end_vertex = edge.verts
+
+        if side_check(start_vertex) or side_check(end_vertex):
+            deselect_edge_and_associated_vertices(edge, start_vertex, end_vertex)
+
+        else:
+            pass
+
+    for vertex in mesh.verts:
+        if side_check(vertex):
+            vertex.select = False
+
+
+def align_right_lens_area_and_bridge(right_lens_area_object, bridge_object, gap):
+    align_lens_area_and_bridge(right_lens_area_object, bridge_object)
+
+    select_object(right_lens_area_object)
     bpy.ops.transform.translate(value=(gap*2, 0, 0), constraint_axis=(True, False, False))
+
+
+def align_lens_area_and_bridge(lens_object, bridge_object):
+    select_object(bridge_object)
+    select_second_object(lens_object)
+    bpy.ops.object.align(bb_quality=True, align_mode='OPT_3', relative_to='OPT_1', align_axis={'Y'})
 
 
 def align(left_lens_object, right_lens_object, bridge_object, gap):
@@ -702,98 +421,11 @@ def create_eyeglasses_from_svg(desired_width=135, desired_thickness=4.5, bridge_
 
     reorient_for_easier_manipulation(selected_object)
 
-    form_lens_and_bridge(bridge_width, bend_degree)
+    frame_object = form_lens_and_bridge(bridge_width, bend_degree)
 
-    #create_nosepads(selected_object)
+    #create_nosepads(frame_object)
+
+    #protrude_bridge(frame_object)
 
 
 create_eyeglasses_from_svg()
-
-
-def run(width=135, protruded_bridge=True, thickness=4.2):
-    """
-    width is the width of the frame, in mm
-    thickness is the thickness of the frame, in mm
-    protruded_bridge is a toggle for where the bridge should be protruded
-    """
-
-    setup_environment()
-
-    #imported SVG of eyeglasses frame
-    selected_object = bpy.context.scene.objects.active
-
-    mesh = create_mesh_from_svg(selected_object)
-
-    #fancy display to better visualize the changes
-    selected_object.data.materials[0].diffuse_color = (1.0, 1.0, 1.0)
-
-    #since Bend only bends around the Z axis, the frame have to be rotated.
-    rotate_object()
-
-    #for some reason there's a translation that happens that offsets the object origin
-    move_object_origin_to_center_of_mass()
-
-    #scale the frame for easier manipulation
-    scale_for_manipulation()
-
-    #move to global origin so that it's easy to know where the center is
-    move_to_origin(selected_object)
-    
-    #removes duplicate vertexes (like artifacts from converting paths to mesh and beveling)
-    #which also welds the seams
-    remove_duplicate_vertices()
-    
-    half_length = find_half_length(mesh)
-    
-    bridge_area = find_bridge_area(half_length)
-
-    #bisect middle to lessen artifacts from creating nosepads
-    #the bisect_bridge_area method isn't called here, because that increases artifacts when scaling the nosepads
-    #it's a delicate balance
-    bisect_origin()
-    
-    #calculate information necessary for nosepads
-    nosepad_z_height_range, normal_vertices, nosepad_vertices = find_nosepads_values(mesh, bridge_area)
-    
-    right_nosepad_vertex_indices, left_nosepad_vertex_indices = nosepad_vertices
-    nosepad_peak_z_height_range = find_nosepad_peak_height(nosepad_z_height_range)
-    
-    #create left nosepad
-    select_all_nosepad_vertices(right_nosepad_vertex_indices, nosepad_z_height_range, mesh)
-    shrink_nosepad()
-    select_nosepad_peak_vertices(nosepad_peak_z_height_range, right_nosepad_vertex_indices, mesh)
-    extrude_nosepad_peak()
-    
-    #create right nosepad
-    select_all_nosepad_vertices(left_nosepad_vertex_indices, nosepad_z_height_range, mesh)
-    shrink_nosepad()
-    select_nosepad_peak_vertices(nosepad_peak_z_height_range, left_nosepad_vertex_indices, mesh)
-    extrude_nosepad_peak()
-
-    #reset all undesired manipulations from proportional editing
-    reset_normal_vertices(normal_vertices, mesh)
-
-    #bisect the bridge area to reduce artifacts from bending as well as protrusion
-    bisect_bridge_area(bridge_area)
-
-    #bisect the area around each lens to lessen the artifacts from bending
-    mid_lens_coord = find_mid_lens_coord(half_length)
-    bisect_mid_lens_areas(mid_lens_coord, half_length)
-
-    #bend the frame slightly around each lens
-    bend_object(selected_object)
-        
-    if protruded_bridge:
-        #protrude the upper bridge region
-        select_mid_bridge_points(mesh)
-        protrude_bridge()
-
-    scale_to_lifesize(half_length, width)
-
-    #remove artifacts / cleanup
-    remove_duplicate_vertices()
-    
-    #end in object mode for better viewing
-    bpy.ops.object.mode_set(mode="OBJECT")
-
-    print("O-O success!")
